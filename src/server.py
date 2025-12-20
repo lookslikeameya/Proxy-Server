e1c321ae2c44ac66d87f5e9ebd5872ebbcad1b4f
Ameya Gupta <ameya7072@gmail.com>
2025-12-21 05:19:53 +0530

    made parser function for the rquests....

--- src/server.py
+++ src/server.py
@@ -4,14 +4,59 @@ import threading
 HOST = "127.0.0.1"
 PORT = 8888
 
+def parse_http_request(request_bytes):
+    text = request_bytes.decode(errors="ignore")
+    lines = text.split("\r\n")
+
+    # Request line
+    request_line = lines[0]
+    method, target, version = request_line.split()
+
+    host = None
+    port = 80
+    path = "/"
+
+    # Case 1: Absolute URL (proxy style)
+    if target.startswith("http://"):
+        without_http = target[len("http://"):]
+        parts = without_http.split("/", 1)
+
+        host_port = parts[0]
+        path = "/" + parts[1] if len(parts) > 1 else "/"
+
+        if ":" in host_port:
+            host, port = host_port.split(":")
+            port = int(port)
+        else:
+            host = host_port
+
+    # Case 2: Relative path + Host header
+    else:
+        path = target
+        for line in lines:
+            if line.lower().startswith("host:"):
+                host = line.split(":", 1)[1].strip()
+                break
+
+    return method, host, port, path
+
 
 def handle_client(client_socket, client_address):
     print(f"[+] Handling {client_address}")
 
     try:
         data = client_socket.recv(4096)
+        method, host, port, path = parse_http_request(data)
+
+       
+        
+
         print("----- RAW DATA START -----")
-        print(data.decode(errors="ignore"))
+        print("METHOD:", method)
+        print("HOST  :", host)
+        print("PORT  :", port)
+        print("PATH  :", path)
+
         print("----- RAW DATA END -----")
 
         body = "Hello from proxy server!\n"
